6a39a40601fa5a3887976a218ca92ef57479c59d
Jonathan Mamdouh Monir <2021170148@cis.asu.edu.eg>
2025-04-02 22:33:00 +0200

    add possession calculator code

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import json
 from tracker import process_video
 from cluster_time_improve import main_multi_frame
 from transformation import process_field_transformation
+from possessionCalculation import CaculatePossession
 
 # TRACKING
 results = process_video(r"resources\yolo8.pt", r"resources\manc2.mp4", 20)
@@ -26,9 +27,11 @@ results = process_field_transformation(results_with_class_ids, calibrator_cfgs)
 
 
 
+yardTL, yardTR, yardBL, yardBR = [29.0, 17.0], [45.5, 17.0], [29.0, 26.0], [45.5, 26.0]
+poss = CaculatePossession(results, yardTL, yardTR, yardBL, yardBR)
+print(poss)
 
-
-json_output_path = r"results_field_transformed.json"
-with open(json_output_path, "w") as f:
-    json.dump(results, f, indent=4)
-print(f"Results saved to {json_output_path}")
+# json_output_path = r"results_field_transformed.json"
+# with open(json_output_path, "w") as f:
+#     json.dump(results, f, indent=4)
+# print(f"Results saved to {json_output_path}")
---
+++ possessionCalculation.py
@@ -0,0 +1,143 @@
+def CaculatePossession(data, yardTL = [0.0, 68.0], yardTR=[105.0, 68.0], yardBL=[0.0, 0.0], yardBR=[105.0, 0.0]):
+    frames = 0
+    framesT1 = framesT2 = 0
+    cumulative_possessions = []
+    prevBall = None
+    for i, frame in enumerate(data):
+        # i = frame["frame_index"]
+        distances = { "T1" : [], "T2" : [] }
+        players = frame["players"]
+        ball = frame["ball"]
+
+        if players is None or (i == 0 and ball is None):
+            cumulative_possessions.append(GetPossessionPercentage(frames, framesT1, framesT2))
+            continue
+        prevBall_position = prevBall["field_position"] if prevBall is not None else None
+        if isinstance(ball, list) and len(ball) > 1:
+            if prevBall is not None and prevBall_position[0] >= yardTL[0] and prevBall_position[0] >= yardTR[0] and prevBall_position[1] >= yardTL[1] and prevBall_position[1] <= yardBL[1]:
+                currBall = ball[0]
+                minDist = DistanceBetweenObjects(currBall["field_position"], prevBall_position)
+                for idx, b in enumerate(ball):
+                    if idx == 0:
+                        continue
+                    b_position = b["field_position"]
+                    if b_position[0] >= yardTL[0] and b_position[0] >= yardTR[0] and b_position[1] >= yardTL[1] and b_position[1] <= yardBL[1]:
+                        d = DistanceBetweenObjects(b_position, prevBall_position)
+                        if d < minDist:
+                            minDist = d
+                            currBall = b
+                    else:
+                        ball.pop(idx)
+                ball = currBall
+            else:
+                # ANY THING
+                ball = ball[0]
+
+        if i > 0 and frame["ball"] is None:
+            if prevBall is not None and prevBall_position[0] >= yardTL[0] and prevBall_position[0] >= yardTR[0] and prevBall_position[1] >= yardTL[1] and prevBall_position[1] <= yardBL[1]:
+                ball = prevBall
+            else:
+                cumulative_possessions.append(GetPossessionPercentage(frames, framesT1, framesT2))
+                continue
+
+        for player in players:
+            if player["class_id"] == 1:
+                distances["T1"].append(DistanceBetweenObjects(player["field_position"], ball["field_position"]))
+            elif player["class_id"] == 2:
+                distances["T2"].append(DistanceBetweenObjects(player["field_position"], ball["field_position"]))
+
+        prevBall = ball
+
+        distancesT1 = len(distances["T1"])
+        distancesT2 = len(distances["T2"])
+        if distancesT1 == 0 and distancesT2 == 0:
+            cumulative_possessions.append(GetPossessionPercentage(frames, framesT1, framesT2))
+            continue
+        if distancesT1 == 0:
+            framesT2 += 1
+        elif distancesT2 == 0:
+            framesT1 += 1
+        elif min(distances["T1"]) < min(distances["T2"]):
+            framesT1 += 1
+        else:
+            framesT2 += 1
+        frames += 1
+        cumulative_possessions.append(GetPossessionPercentage(frames, framesT1, framesT2))
+    return cumulative_possessions
+
+def DistanceBetweenObjects(player, ball):
+    return ((player[0] - ball[0])**2 + (player[1] - ball[1])**2)**0.5
+
+
+def GetPossessionPercentage(frames, framesT1, framesT2):
+    if frames == 0:
+        return { "possT1" : 0, "possT2" : 0 }
+    possT1 = (framesT1 / frames) * 100
+    possT2 = (framesT2 / frames) * 100
+    return { "possT1" : possT1, "possT2" : possT2 }
+
+
+
+# data = [
+#     {
+#         "frame_index": 0,
+#         "players": [
+#             {"class_id": 1, "field_position": [62.53, 43.14]},
+#             {"class_id": 1, "field_position": [91.73, 35.23]},
+#             {"class_id": 1, "field_position": [60.05, 36.33]},
+#             {"class_id": 2, "field_position": [66.44, 51.01]},
+#             {"class_id": 2, "field_position": [57.66, 40.67]},
+#             {"class_id": 1, "field_position": [68.74, 23.63]},
+#             {"class_id": 1, "field_position": [70.96, 41.67]},
+#             {"class_id": 2, "field_position": [65.72, 16.64]},
+#             {"class_id": 2, "field_position": [52.59, 38.76]},
+#             {"class_id": 2, "field_position": [72.84, 36.52]}
+#         ],
+#         "ball": {"class_id": 0, "field_position": [62.53, 43.14]}
+#     },
+#     {
+#         "frame_index": 1,
+#         "players": [
+#             {"class_id": 1, "field_position": [62.75, 43.50]},
+#             {"class_id": 1, "field_position": [91.50, 35.10]},
+#             {"class_id": 1, "field_position": [60.20, 36.50]},
+#             {"class_id": 2, "field_position": [66.30, 51.20]},
+#             {"class_id": 2, "field_position": [57.50, 40.80]},
+#             {"class_id": 1, "field_position": [68.90, 23.80]},
+#             {"class_id": 1, "field_position": [71.10, 41.90]},
+#             {"class_id": 2, "field_position": [65.80, 16.80]},
+#             {"class_id": 2, "field_position": [52.40, 38.50]},
+#             {"class_id": 2, "field_position": [72.50, 36.90]}
+#         ],
+#         "ball": [
+#             {"class_id": 0, "field_position": [78.10, 29.00]},
+#             {"class_id": 0, "field_position": [56.00, 44.80]}
+#         ]
+#     },
+#     {
+#         "frame_index": 2,
+#         "players": [
+#             {"class_id": 1, "field_position": [63.00, 43.80]},
+#             {"class_id": 1, "field_position": [91.20, 35.00]},
+#             {"class_id": 1, "field_position": [60.40, 36.60]},
+#             {"class_id": 2, "field_position": [66.10, 51.40]},
+#             {"class_id": 2, "field_position": [57.30, 40.90]},
+#             {"class_id": 1, "field_position": [69.10, 24.00]},
+#             {"class_id": 1, "field_position": [71.30, 42.10]},
+#             {"class_id": 2, "field_position": [65.90, 17.00]},
+#             {"class_id": 2, "field_position": [52.20, 38.30]},
+#             {"class_id": 2, "field_position": [72.20, 37.20]}
+#         ],
+#         "ball": [
+#             {"class_id": 0, "field_position": [78.80, 29.50]},
+#             {"class_id": 0, "field_position": [56.80, 44.20]}
+#         ]
+#     }
+# ]
+# data = None
+
+
+
+# yardTL, yardTR, yardBL, yardBR = [29.0, 17.0], [45.5, 17.0], [29.0, 26.0], [45.5, 26.0]
+# poss = CaculatePossession(data, yardTL, yardTR, yardBL, yardBR)
+# print(poss)
