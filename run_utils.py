ee0833c7d04c2ab40df30e327c56b912e0511109
Jonathan Mamdouh Monir <2021170148@cis.asu.edu.eg>
2025-05-23 13:41:21 +0300

    dimension fix

--- run_utils.py
+++ run_utils.py
@@ -961,7 +961,7 @@ def get_detections(yolov11_detector, frame: np.ndarray, class_id: int, confidenc
         List of detections for the specified class.
     """
     h, w = frame.shape[:2]
-    imgsz = max(h, w)
+    imgsz = 800
      
     imgsz = adjust_imgsz(imgsz)  # or imgsz = adjust_imgsz(imgsz)
     results = yolov11_detector.predict(frame, imgsz=imgsz, verbose=False)
